6141f87a446875f930bb85224cc58ff3b8a7f9b5
Richie Wong <richiewong07@yahoo.com>
2017-10-10 11:09:38 -0500

    Need to cache

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import tornado.log
 
 import requests
 
+
 from jinja2 import \
   Environment, PackageLoader, select_autoescape
 
@@ -18,29 +19,49 @@ class TemplateHandler(tornado.web.RequestHandler):
   def render_template (self, tpl, context):
     template = ENV.get_template(tpl)
     self.write(template.render(**context))
-    
+
 class MainHandler(TemplateHandler):
   def get (self):
-    # render input form
-    pass
-    
+    template = ENV.get_template('index.html')
+    self.write(template.render())
+
+class WeatherHandler(TemplateHandler):
+  def get (self):
+    template = ENV.get_template('weather.html')
+    self.write(template.render())
+
   def post (self):
-    pass
-    # get city name
+    city = self.get_body_argument('city')
+
+
+    url = "http://api.openweathermap.org/data/2.5/weather"
+
+    querystring = {"q":city,"APIKEY":"2d9a500b15c2b6ea12cd0298f2df8696",'units':'imperial'}
+
+    headers = {
+        'cache-control': "no-cache",
+        'postman-token': "93bd509e-5f98-e8b9-540d-2c8a019ee099"
+        }
+
+    r = requests.request("GET", url, headers=headers, params=querystring)
     
-    # lookup the weather
-    
-    # render the weather data
-    
+    r.json()
+
+    template = ENV.get_template('weather.html')
+
+    self.write(template.render({'data': r.json()}))
+
+
 def make_app():
   return tornado.web.Application([
     (r"/", MainHandler),
-    (r"/static/(.*)", 
+    (r"/weather", WeatherHandler),
+    (r"/static/(.*)",
       tornado.web.StaticFileHandler, {'path': 'static'}),
   ], autoreload=True)
-  
+
 if __name__ == "__main__":
   tornado.log.enable_pretty_logging()
   app = make_app()
-  app.listen(int(os.environ.get('PORT', '8080')))
+  app.listen(int(os.environ.get('PORT', '8888')))
   tornado.ioloop.IOLoop.current().start()
